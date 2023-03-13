323e9572219943badaca7f727ed9e543250df1a2
TeoSean00 <teosean@outlook.com>
2023-03-13 23:10:55 +0800

    profile page styling and responsiveness completed

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -15,47 +15,83 @@ function ProfilePage() {
 
   return (
     <Layout user={state}>
-      <div className="container mx-auto max-w-4xl bg-slate-100 p-8">
+      <div className="container mx-auto max-w-4xl bg-gray-800 p-8">
         <div className="mb-8 flex flex-col gap-y-8">
-          <div className="flex flex-row justify-start">
-            <div className="flex basis-2/5 flex-col">
-              <img
-                className="rounded-3xl object-fill"
-                src={`${state.picture}`}
-              />
+            {/* Only appear below medium viewports */}
+            <div className="flex flex-row justify-start">
+                <div className="flex flex-col items-center basis-full md:basis-0">
+                    <img
+                        className="rounded-3xl object-fill"
+                        src={`${state.picture}`}
+                        alt=""
+                    />
+                </div>
             </div>
-            <div className="flex basis-3/5 flex-col justify-evenly px-12">
-              <div className="flex flex-row justify-start">
-                <h1 className="font-bold">Name: {state.name}</h1>
-              </div>
-              <div className="flex flex-row justify-start">
-                <h1 className="font-bold">Email: {state.email}</h1>
-              </div>
-              <div className="flex flex-row justify-start">
-                <h1 className="font-bold">Your Interests:</h1>
-              </div>
+            <div className="flex flex-row justify-start">
+                {/* Only appaer medium viewports and above */}
+                <div className="flex flex-col basis-0 md:basis-2/5">
+                    <img
+                        className="rounded-3xl object-fill"
+                        src={`${state.picture}`}
+                        alt=""
+                    />
+                </div>
+                <div className="flex flex-col basis-full md:basis-3/5 justify-evenly md:px-12">
+                    <div className="flex flex-row justify-start">
+                        <h1 className="font-bold text-gray-300">Name: {state.name}</h1>
+                    </div>
+                    <div className="flex flex-row justify-start">
+                        <h1 className="font-bold text-gray-300">Email: {state.email}</h1>
+                    </div>
+                    <div className="flex flex-col">
+                        <div className="flex flex-row">
+                            <h1 className="font-bold text-gray-300 mb-2">Your Interests:</h1>
+                        </div>
+                        <div className="flex flex-wrap justify-evenly">
+                            <div className="flex rounded-3xl bg-gray-700 p-3 m-1 items-center">
+                                <p className="text-lg text-gray-300">Pilates</p>
+                            </div>
+                            <div className="flex rounded-3xl bg-gray-700 p-3 m-1 items-center">
+                                <p className="text-lg text-gray-300">FrontEnd</p>
+                            </div>
+                            <div className="flex rounded-3xl bg-gray-700 p-3 m-1 items-center">
+                                <p className="text-lg text-gray-300">Health</p>
+                            </div>
+                            <div className="flex rounded-3xl bg-gray-700 p-3 m-1 items-center">
+                                <p className="text-lg text-gray-300">Cooking</p>
+                            </div>
+                            <div className="flex rounded-3xl bg-gray-700 p-3 m-1 items-center">
+                                <p className="text-lg text-gray-300">Reading</p>
+                            </div>
+                            <div className="flex rounded-3xl bg-gray-700 p-3 m-1 items-center">
+                                <p className="text-lg text-gray-300">BackEnd</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
+
           <div className="flex flex-col">
             <div className="flex flex-row">
-              <h1 className="font-bold">Your Registered Classes:</h1>
+              <h1 className="font-bold text-gray-300">Your Registered Classes:</h1>
             </div>
             <div className="flex flex-row">
-              <div className="mt-2 block h-full w-full rounded-3xl bg-blue-200 p-6">
-                <p className="text-lg">
+              <div className="mt-2 block h-full w-full rounded-3xl bg-gray-700 p-6">
+                <p className="text-lg text-gray-300">
                   Here are the biggest enterprise technology acquisitions of
                   2021 so far, in reverse chronological order.
                 </p>
               </div>
             </div>
           </div>
+          
           <div className="flex flex-col">
             <div className="flex flex-row">
-              <h1 className="font-bold">Your Reviews:</h1>
+              <h1 className="font-bold text-gray-300">Your Reviews:</h1>
             </div>
             <div className="flex flex-row">
-              <div className="mt-2 block h-full w-full rounded-3xl bg-blue-200 p-6">
-                <p className="text-lg">
+              <div className="mt-2 block h-full w-full rounded-3xl bg-gray-700 p-6">
+                <p className="text-lg text-gray-300">
                   Here are the biggest enterprise technology acquisitions of
                   2021 so far, in reverse chronological order.
                 </p>
