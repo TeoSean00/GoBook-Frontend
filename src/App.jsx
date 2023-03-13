2c51dd89145332fbb92348bf95a254c195dc6a04
JosephJL <HeeJLJoseph@gmail.com>
2023-03-13 16:37:41 +0800

    Stripe Payment Integration

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,16 @@ import { GoogleLogin, googleLogout, useGoogleLogin } from "@react-oauth/google";
 import Layout from "./components/Layout";
 import toast, { Toaster } from "react-hot-toast";
 
+//Stuff for payments page
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckoutForm from "./CheckoutForm";
+import "./App.css";
+
+const stripePromise = loadStripe(
+  "pk_test_51MkznJJTqG9NvRuTCvhU1y4RyggSstQYI2woG0L2DQywIKMFmvYVSqyS6uwHfCsK1mdv5Nvo6KP1NzAnR0wlukX900AB3llvRf"
+);
+
 function App() {
   const [user, setUser] = useState(null);
   const handleFetch = async (codeResponse) => {
@@ -46,6 +56,28 @@ function App() {
     localStorage.clear();
   };
 
+  //stripe stuff testing
+  const [clientSecret, setClientSecret] = useState("");
+
+  useEffect(() => {
+    // Create PaymentIntent as soon as the page loads
+    fetch("http://localhost:8080/create-payment-intent", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ items: [{ id: "xl-tshirt" }] }),
+    })
+      .then((res) => res.json())
+      .then((data) => setClientSecret(data.clientSecret));
+  }, []);
+
+  const appearance = {
+    theme: "stripe",
+  };
+  const options = {
+    clientSecret,
+    appearance,
+  };
+
   return (
     <React.StrictMode>
       <Toaster />
@@ -54,6 +86,12 @@ function App() {
           <Layout>'Hello {user.name}'</Layout>
         ) : (
           <Login handleLogin={handleLogin} />
+        )}
+
+        {clientSecret && (
+          <Elements options={options} stripe={stripePromise}>
+            <CheckoutForm />
+          </Elements>
         )}
       </div>
     </React.StrictMode>
