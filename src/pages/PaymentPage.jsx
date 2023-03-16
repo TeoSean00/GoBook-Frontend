2e2cec018c6cc646f47d7226c4f44771cdd9da3b
JosephJL <HeeJLJoseph@gmail.com>
2023-03-16 12:19:33 +0800

    Update PaymentPage.jsx

--- src/pages/PaymentPage.jsx
+++ src/pages/PaymentPage.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 
-
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 import CheckoutForm from "./CheckoutForm";
@@ -11,36 +10,36 @@ const stripePromise = loadStripe(
 );
 
 export default function PaymentPage() {
-    const [clientSecret, setClientSecret] = useState("");
+  const [clientSecret, setClientSecret] = useState("");
 
-    useEffect(() => {
-        // Create PaymentIntent as soon as the page loads
-        fetch("http://localhost:8080/create-payment-intent", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ items: [{ id: "xl-tshirt" }] }),
-        })
-        .then((res) => res.json())
-        .then((data) => setClientSecret(data.clientSecret));
-    }, []);
+  useEffect(() => {
+    // Create PaymentIntent as soon as the page loads
+    fetch("http://localhost:8080/create-payment-intent", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ items: [{ id: "xl-tshirt" }] }),
+    })
+      .then((res) => res.json())
+      .then((data) => setClientSecret(data.clientSecret));
+  }, []);
 
-    const appearance = {
-        theme: "stripe",
-    };
-    const options = {
-        clientSecret,
-        appearance,
-    };
+  const appearance = {
+    theme: "stripe",
+  };
+  const options = {
+    clientSecret,
+    appearance,
+  };
 
-    return (
-        <React.StrictMode>
-            <div>
-                {clientSecret && (
-                <Elements options={options} stripe={stripePromise}>
-                    <CheckoutForm />
-                </Elements>
-                )}
-            </div>
-        </React.StrictMode>
-    );
+  return (
+    <React.StrictMode>
+      <div>
+        {clientSecret && (
+          <Elements options={options} stripe={stripePromise}>
+            <CheckoutForm />
+          </Elements>
+        )}
+      </div>
+    </React.StrictMode>
+  );
 }
