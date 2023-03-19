5aeb1e937cd6db67379d1a192fcd81b811d24c20
Jerome Wong <jeromewongjh@gmail.com>
2023-03-19 15:30:33 +0800

    fix: resolve button color prop bug

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,14 @@
 import React from "react";
-
-const Button = ({ name, onClick, color = "blue" }) => {
+import classnames from "classnames";
+const Button = ({ name, onClick, color }) => {
+  const colorVariants = {
+    blue: "bg-blue-700 hover:bg-blue-800 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800",
+    red: "bg-red-700 hover:bg-red-800 focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800",
+    gray: "bg-gray-700 hover:bg-gray-800 focus:ring-gray-300 dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800",
+  };
   return (
     <button
-      type="button"
-      className={`w-fit rounded-lg bg-${color}-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-${color}-800 focus:outline-none focus:ring-4 focus:ring-${color}-300 dark:bg-${color}-600  dark:hover:bg-${color}-700 dark:focus:ring-${color}-800`}
+      className={`w-fit rounded-lg px-5 py-2.5 text-sm font-medium text-gray-50  focus:outline-none focus:ring-4 ${colorVariants[color]}`}
       onClick={onClick}
     >
       {name}
