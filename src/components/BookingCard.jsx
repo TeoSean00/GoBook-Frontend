6f487173c93918b10b20506fe04efb65c18cb8fa
Tyler Lian <65641550+anahopbob@users.noreply.github.com>
2023-03-26 13:57:06 +0800

    Calendar

--- src/components/BookingCard.jsx
+++ src/components/BookingCard.jsx
@@ -1,60 +1,98 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import 'react-calendar/dist/Calendar.css';
 import Calendar from 'react-calendar'
 import Button from './Button';
 
 const BookingCard = () => {
   const [value, onChange] = useState(new Date());
-  const [selectedDate, setSelectedDate] = useState("");
-  const [selectedTime, setSelectedTime] = useState("");
+  const [selectedDate, setSelectedDate] = useState('default');
+  const [selectedTime, setSelectedTime] = useState('default');
+  const [availableDate, setAvailableDate] = useState([]);
   const [availableTime, setAvailableTime] = useState([]);
-  const selectDate = async (event) => {
-    const option = event.target.value
-    await setSelectedDate(option);
-    course.classes.forEach(async element => {
-      if (element.date === selectedDate){
-        await setAvailableTime([...element.timeslot]);
-        console.log(availableTime)
+
+  const setDates = () =>{
+    var times = [];
+    course.classes.forEach(element => {
+      if (!times.includes(element.date)){
+        times.push(element.date);
+      }
+    })
+    return times;
+  }
+  // On Page Load
+  useEffect(()=>{
+    var availableDates = setDates();
+    setAvailableDate([...availableDates])
+  }, [])
+  // Checking
+  useEffect(()=>{
+    console.log(selectedTime + "Date Changed")
+  }, [selectedTime]);
+
+  // Calendar selection
+  useEffect(() => {
+    selectDate(value);
+  }, [value]);
+  
+    
+  const selectDate = (val) => {
+    var availableTimeslots = [];
+    const selectedDay = val.getDate();
+    const selectedMonth = val.getMonth();
+    course.classes.forEach(element => {
+      var classDate = new Date(element.date);
+      if(classDate.getDate() === selectedDay && classDate.getMonth() === selectedMonth){
+        availableTimeslots.push(element.timeslot);
       }
     });
+    setSelectedDate(val.getFullYear() + '-' + val.getMonth() + '-' + val.getDate());
+    setAvailableTime([...availableTimeslots])
   }
-  const submitBooking = (course, date, time) => {
-    console.log()
+
+  const selectTime = (event) => {
+    setSelectedTime(event.target.value)
+  };
+
+  const submitBooking = () => {
+    alert(course.className + selectedDate + selectedTime)
   }
+
   return (
     <div>
       <Calendar onChange={onChange} value={value}/>
       <span className='bg-white'>
-        Date selected: {selectedDate}
+        Date selected: {value.getDate()}/{value.getMonth()}/{value.getFullYear()}
       </span>
       <br />
-      <span className='bg-white'>
-        Select Your Date: 
-      </span>
-      <select className="form-select" onChange={selectDate}>
-        {course.classes.map(option => {
-            return (
-              <option value={option.date} key={option.date}> 
-                {option.date}
-              </option>
-            );
-          })} 
-      </select>
+      {availableTime.length === 0 &&
+        <span className='bg-white'>
+          There are no available time slots for selected day
+        </span>
+      }
+      {availableTime.length > 0 && (
+        <div>
+          <span className='bg-white'>
+            Select Your TimeSlot: 
+          </span>
+          <select className="form-select" onChange={selectTime} value={selectedTime}>
+            <option disabled value={'default'}>
+              Choose a time slot
+            </option>
+            {availableTime.map(option => {
+                return  (
+                  <option value={option} key={option}>
+                    {option}
+                  </option>
+                );
+              })} 
+          </select>
+        </div>
+      )}
       <br />
-      <span className='bg-white'>
-        Select Your TimeSlot: 
-      </span>
-      <select className="form-select">
-        {availableTime.map(option => {
-            return  (
-              <option value={option} key={option}>
-                {option}
-              </option>
-            );
-          })} 
-      </select>
-      <br />
-      <Button name="Book Your Class" onClick={submitBooking(course, selectedDate, selectedTime)} color={"blue"} />
+      {selectedTime !== 'default' &&
+        <Button name="Book Your Class" onClick={submitBooking} color={"blue"} />
+      }
+      
     </div>
   );
 }
@@ -67,20 +105,20 @@ const course =
       " Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.",
     classes: [
       {
-        date: "2023-2-24",
-        timeslot: ['11.00am - 11.30am', '12.00am - 12.30am']
+        date: "2023-3-24",
+        timeslot: "11.00am - 12.00pm",
       },
       {
-        date: "2023-2-25",
-        timeslot: ['11.02am - 11.32am', '12.00am - 12.30am']
+        date: "2023-3-24",
+        timeslot: "4.00pm - 5.00pm",
       },
       {
-        date: "2023-2-26",
-        timeslot: ['11.05am - 11.35am', '12.00am - 12.30am']
+        date: "2023-3-26",
+        timeslot: "12.00am - 1.00pm"
       },
       {
-        date: "2023-2-27",
-        timeslot: ['11.09am - 11.39am', '12.00am - 12.30am']
+        date: "2023-3-26",
+        timeslot: "5.00pm - 6.00pm",
       }
     ]
   }
