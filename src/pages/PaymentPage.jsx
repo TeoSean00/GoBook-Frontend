f741c77953b71e5b4c61156e4408972ab38542aa
JosephJL <HeeJLJoseph@gmail.com>
2023-04-03 22:03:32 +0800

    Update PaymentPage.jsx

--- src/pages/PaymentPage.jsx
+++ src/pages/PaymentPage.jsx
@@ -47,7 +47,7 @@ export default function PaymentPage() {
     };
   }, []);
 
-  function selectClass() {
+  function makePayment() {
     fetch("http://localhost:5008/booking/createPayment", {
       method: "POST",
       mode: "cors",
@@ -60,28 +60,6 @@ export default function PaymentPage() {
     console.log(data);
   }
 
-  const [count, setCount] = useState(0);
-
-  var handleClick = () => {
-    setCount(count + 1);
-  };
-
-  function MyButton() {
-    return <button onClick={handleClick}>I was clicked {count} times!</button>;
-  }
-
-  // useEffect(() => {
-  //   // Create PaymentIntent as soon as the page loads
-  //   fetch("http://localhost:5008/booking/createPayment", {
-  //     method: "POST",
-  //     mode: "cors",
-  //     headers: { "Content-Type": "application/json" },
-  //     body: JSON.stringify(purchase),
-  //   })
-  //     .then((res) => res.json())
-  //     .then((data) => setClientSecret(data.clientSecret));
-  // }, []);
-
   const appearance = {
     theme: "stripe",
   };
@@ -93,12 +71,11 @@ export default function PaymentPage() {
   return (
     <React.StrictMode>
       <div>
-        <h2>{data.courseName}</h2>
         <MyButton />
         <button
           type="button"
           className="mr-2 mb-2 rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          onClick={selectClass}
+          onClick={makePayment}
         >
           Select Class
         </button>
