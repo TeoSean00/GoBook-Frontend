e441a7766ca2af62ffc909dbc0813f09914a3bef
JosephJL <HeeJLJoseph@gmail.com>
2023-04-04 14:09:13 +0800

    Update PaymentPage.jsx

--- src/pages/PaymentPage.jsx
+++ src/pages/PaymentPage.jsx
@@ -71,7 +71,6 @@ export default function PaymentPage() {
   return (
     <React.StrictMode>
       <div>
-        <MyButton />
         <button
           type="button"
           className="mr-2 mb-2 rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
